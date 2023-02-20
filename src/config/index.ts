667aca317b6d1cfca30a4ced4dffb5998e2f4b82
hahs <alexandre_2420@outlook.com>
2023-02-20 17:10:16 -0500

    added: rabbit connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,3 +11,5 @@ export const configuration = {
 
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+export const RABBIT_URL = process.env.RABBIT_URL || "";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
+import * as amqp from "amqplib/callback_api";
+
 import { AppDataSource } from "./database/dataSource";
+import { RABBIT_URL } from "./config";
 import app from "./app";
 
 async function main() {
   try {
     await AppDataSource.initialize();
-    app.listen(5000, () => {
-      console.log("server listening...");
+
+    //rabbit connection
+    amqp.connect(RABBIT_URL, (err0, connection) => {
+      if (err0) {
+        throw err0;
+      }
+
+      connection.createChannel((err1, channel) => {
+        if (err1) {
+          throw err1;
+        }
+        app.listen(5000, () => {
+          console.log("server listening...");
+        });
+      });
     });
   } catch (error) {
     console.error(error);
